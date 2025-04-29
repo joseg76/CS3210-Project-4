7f7cd1872183645dea8772c5d10ef29825dbe915
josegomez <pavelama277@gmail.com>
2025-04-29 15:16:02 -0600

    First 3

---
+++ project4_1.lisp
@@ -0,0 +1,22 @@
+(defun set-member (set item)
+    (if (null set)
+        nil
+        (if (equal (car set) item)
+            t
+            (set-member (cdr set) item))))
+
+(defun set-union (set-1 set-2)
+  (if (null set-2)
+      set-1
+      (let ((item (car set-2)))
+        (if (set-member set-1 item)
+            (set-union set-1 (cdr set-2))
+            (set-union (cons item set-1) (cdr set-2))))))
+
+(defun set-intersection (set-1 set-2)
+  (if (null set-1)
+      nil
+      (let ((item (car set-1)))
+        (if (set-member set-2 item)
+            (cons item (set-intersection (cdr set-1) set-2))
+            (set-intersection (cdr set-1) set-2)))))
