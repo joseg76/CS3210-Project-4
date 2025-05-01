83b34a7f3a87f696462a90e6dfbc3557c82bc05e
Isaac Lee <123617368+GreyGreg15@users.noreply.github.com>
2025-05-01 14:27:54 -0600

    Update project4.lisp

--- project4.lisp
+++ project4.lisp
@@ -28,3 +28,8 @@
       (if (set-member set-2 item)
         (set-diff (cdr set-1) set-2)
         (cons item (set-diff (cdr set-1) set-2))))))
+
+(defun boolean-xor (a b)
+    (if (equal a b)
+    nil
+    t))
